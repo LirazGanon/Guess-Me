c8a668f7ba916e64cd6a7341db82d1c7f3cd26b2
Liraz Ganon <105818149+LirazGanon@users.noreply.github.com>
2022-10-12 09:32:20 +0300

    Add files via upload

--- js/storage.service.js
+++ js/storage.service.js
@@ -1,14 +1,17 @@
 'use strict'
 
 let req = new XMLHttpRequest();
+var gRes
+
+req.onreadystatechange = () => {
+    if (req.readyState == XMLHttpRequest.DONE) {
+        console.log(JSON.parse(req.responseText).record);
+        gRes =JSON.parse(req.responseText).record;
+    }
+};
 
 req.open("GET", "https://api.jsonbin.io/v3/b/6346563365b57a31e6937523/latest", true);
 req.send();
-
-req.open("PUT", "https://api.jsonbin.io/v3/b/6346563365b57a31e6937523", true);
-req.setRequestHeader("Content-Type", "application/json");
-req.setRequestHeader("X-Master-Key", "$2b$10$YNeBmLMlZltse.0oWPXc6uE5/VqZr57sUe0LgfppjoLUqOo1itzE.");
-req.send('{"name": "Hello World"}');
 
 function saveToStorage(key, val) {
     req.open("PUT", "https://api.jsonbin.io/v3/b/6346563365b57a31e6937523", true);
@@ -18,14 +21,5 @@ function saveToStorage(key, val) {
 }
 
 function loadFromStorage(key) {
-    req.open("GET", "https://api.jsonbin.io/v3/b/6346563365b57a31e6937523/latest", true);
-    req.send();
-    var res
-    req.onreadystatechange = () => {
-        if (req.readyState == XMLHttpRequest.DONE) {
-            console.log(JSON.parse(req.responseText).record);
-            res =JSON.parse(req.responseText).record;
-        }
-    };
-    return res
+    return gRes
 }
